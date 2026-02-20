9291c1888feb54b12f75357556815b15a8af9ec1
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-02-20 17:13:26 +0200

    add cart service and page scaffold

---
+++ src/app/components/category-admin/category-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-admin',
+  imports: [],
+  templateUrl: './category-admin.html',
+  styleUrl: './category-admin.css',
+})
+export class CategoryAdmin {
+
+}
---
+++ src/app/services/cart.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class CartService { // talk to backend api
+
+  getCart() {
+    // TODO: Implement getCart method
+  }
+
+  addToCart() {
+    // TODO: Implement addToCart method
+  }
+
+  removeFromCart() {
+    // TODO: Implement removeFromCart method
+  }
+}
