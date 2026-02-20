579f0847f7eca0d77ea134cce61f56e8a0406711
Andrew Emad <andrewemad@Andrews-MacBook-Pro.local>
2026-02-20 16:28:32 +0200

    feat: add service

---
+++ src/app/services/book/book-service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  apiUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  testLocalhost(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
