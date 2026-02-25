5bae0171e95fdf192993b39a87bbfa28fba01f37
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-02-26 00:48:41 +0200

    add 404 (not found page)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {NotFound} from "./pages/not-found/not-found";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+
+
+    {path: '**', component: NotFound} //must be at end:  match any wrong path and redirect to 404 page
+];
