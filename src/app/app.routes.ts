d29f44b1c5f350c470962fcfc8f3d689d8ca89d0
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-02-26 20:34:09 +0200

    add admin panel and admin category management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
-import {NotFound} from "./pages/not-found/not-found";
+import {NotFound} from './pages/not-found/not-found';
+import { Admin } from './pages/admin/admin';
+import { CategoryAdmin } from './components/category-admin/category-admin';
 
 export const routes: Routes = [
 
+  {
+    path: 'admin',
+    component: Admin,
+    children: [
+      { path: 'categories', component: CategoryAdmin }
+    // add  admin routes
+    ]
+  },
 
-
-
-    {path: '**', component: NotFound} //must be at end:  match any wrong path and redirect to 404 page
+  {path: '**', component: NotFound} //must be at end:  match any wrong path and redirect to 404 page
 ];
