98ff218a7d8edf3de9f33e78a75dcc83fb405430
Andrew Emad <andrewemad@Andrew.local>
2026-02-28 00:02:43 +0200

    inital pagination

--- src/app/components/pagenation/pagenation.ts
+++ src/app/components/pagenation/pagenation.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-pagenation',
   imports: [],
   templateUrl: './pagenation.html',
-  styleUrl: './pagenation.css',
+  styleUrl: './pagenation.css'
 })
 export class Pagenation {
-
+  totalNumberOfBooks = input<number>(0);
+  currentPage = input<number>(1);
+  pageSize = input<number>(10);
+  numberOfPages = computed(() => {
+    const numberOfPages
+    return 0;
+  });
 }
--- src/app/interfaces/api-response.ts
+++ src/app/interfaces/api-response.ts
@@ -2,4 +2,7 @@ export interface ApiResponse<T>{
   status:string;
   data:T;
   message?:string
+  totalBooks?: number;
+  currentPage?: number;
+  pageSize?: number;
 }
