30d97531b581933969ba7ae9b3f946e9bc34a8f3
“Mohamed <mohamed.kholy2011@gmail.com>
2026-02-27 21:28:51 +0200

    Added Login and Register pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
-import {NotFound} from './pages/not-found/not-found';
+import { Register } from './pages/register/register';
+import { Login } from './pages/login/login';
+import { NotFound } from './pages/not-found/not-found';
 import { Admin } from './pages/admin/admin';
 import { CategoryAdmin } from './components/category-admin/category-admin';
 import { OrderHistory } from './pages/order-history/order-history';
@@ -7,40 +9,44 @@ import { CheckoutComponent } from './pages/checkout/checkout';
 import { OrderConfirmationComponent } from './pages/order-confirmation/order-confirmation';
 import { AdminOrdersComponent } from './pages/admin-orders/admin-orders';
 
-
-  
-
-
 export const routes: Routes = [
-    {
-        path: 'admin/orders',
-        component: AdminOrdersComponent,
-        title: 'Admin Dashboard'
-    },
-    {
-        path: 'checkout',
-        component: CheckoutComponent,
-        title: 'Checkout'
-    },
-    {
-        path: 'order-confirmation/:id',
-        component: OrderConfirmationComponent,
-        title: 'Order Confirmation'
-    },
-    {
-        path: 'order-history',
-        component: OrderHistory,
-        title: 'Order History'
-    },
-    {
-      path: 'admin',
-      component: Admin,
-      children: [
-        { path: 'categories', component: CategoryAdmin }
-      // add  admin routes
-      ]
-    },
-
-    {path: '**', component: NotFound} //must be at end:  match any wrong path and redirect to 404 page
-
+  {
+    path: 'login',
+    component: Login,
+    title: 'Sign In'
+  },
+  {
+    path: 'register',
+    component: Register,
+    title: 'Create Account'
+  },
+  {
+    path: 'admin/orders',
+    component: AdminOrdersComponent,
+    title: 'Admin Dashboard'
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    title: 'Checkout'
+  },
+  {
+    path: 'order-confirmation/:id',
+    component: OrderConfirmationComponent,
+    title: 'Order Confirmation'
+  },
+  {
+    path: 'order-history',
+    component: OrderHistory,
+    title: 'Order History'
+  },
+  {
+    path: 'admin',
+    component: Admin,
+    children: [
+      { path: 'categories', component: CategoryAdmin }
+      // add admin routes
+    ]
+  },
+  { path: '**', component: NotFound } // must be at end: match any wrong path and redirect to 404 page
 ];
