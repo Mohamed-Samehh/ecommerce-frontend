83eef65384cdd56863971e1d56c46e14f6142931
AlaaAbdullah13 <alaaabdallah33339@gmail.com>
2026-02-28 20:18:17 +0200

    feat: Refactor book card component and enhance cart functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Cart } from './pages/cart/cart';
-import { CategoryAdmin } from './components/category-admin/category-admin';
-import { Home } from './pages/home/home';
-import { TestWrapper } from './test-wrapper/test-wrapper';
-import { Footer } from './components/footer/footer';
-import { NotFound } from './pages/not-found/not-found';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cart, CategoryAdmin, Home, Footer, NotFound,TestWrapper],
-
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
